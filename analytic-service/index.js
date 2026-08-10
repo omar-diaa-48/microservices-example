498855b96bd5af7a9f23f45e961d7b3d08ecc4a3
Omar <omardiaaelwakeel@gmail.com>
2026-08-10 14:24:41 +0300

    feat(analytic-service): enhance message handling for payment, order, and email events

--- analytic-service/index.js
+++ analytic-service/index.js
@@ -14,18 +14,46 @@ const run = async () => {
     try {
         await consumer.connect()
         await consumer.subscribe({
-            topic: "payment-successful",
+            topics: ["payment-successful", "order-successful", "email-successful"],
             fromBeginning: true
         })
 
         await consumer.run({
             eachMessage: async ({ topic, partition, message }) => {
-                const value = message.value.toString()
-                const { userId, cart } = JSON.parse(value)
+                switch (topic) {
+                    case "payment-successful": {
+                        const value = message.value.toString()
+                        const { userId, cart } = JSON.parse(value)
 
-                const total = cart.reduce((acc, item) => acc + item.price, 0)
+                        const total = cart.reduce((acc, item) => acc + item.price, 0)
 
-                console.log(`Analytic consumer: User ${userId} paid ${total}`)
+                        console.log(`Analytic consumer: Payment successful: User ${userId} paid ${total}`)
+                    }
+                        break;
+
+                    case "order-successful": {
+                        const value = message.value.toString()
+                        const { userId, orderId } = JSON.parse(value)
+
+                        console.log(`Analytic consumer: Order successful: User ${userId} Order ${orderId}`)
+                    }
+                        break;
+
+                    case "email-successful": {
+                        const value = message.value.toString()
+                        const { userId } = JSON.parse(value)
+
+                        const total = cart.reduce((acc, item) => acc + item.price, 0)
+
+                        console.log(`Analytic consumer: Email successful: User ${userId}`)
+                    }
+                        break;
+
+                    default:
+                        break;
+                }
+
+
             }
         })
     } catch (error) {
